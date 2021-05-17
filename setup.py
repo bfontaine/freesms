2b785a54821d55d8036579f7d3e76c5c9d42d33f
Baptiste Fontaine <b@ptistefontaine.fr>
2021-05-17 20:11:20 +0200

    Goodbye Travis, hello GitHub actions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 # http://stackoverflow.com/a/7071358/735926
 import re
 VERSIONFILE='freesms/__init__.py'
-verstrline = open(VERSIONFILE).read()
+verstrline = open(VERSIONFILE, 'rt', encoding='utf-8').read()
 VSRE = r'^__version__\s+=\s+[\'"]([^\'"]+)[\'"]'
 mo = re.search(VSRE, verstrline, re.M)
 if mo:
@@ -23,16 +23,23 @@ setup(
     license="MIT",
     description='Send SMS with Free Mobile',
     install_requires=["requests"],
-    long_description="""\
-freesms is a Python interface to Free mobile SMS API.""",
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
