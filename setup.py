a5f080ef1f725d63079843d723404e19a5ea7c7c
Baptiste Fontaine <b@ptistefontaine.fr>
2021-05-17 20:07:34 +0200

    bump dev dependencies; test on Python 3.9

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 # -*- coding: UTF-8 -*-
 
-import io
 from distutils.core import setup
 
 # http://stackoverflow.com/a/7071358/735926
 import re
 VERSIONFILE='freesms/__init__.py'
-# In Python 2.x open() doesn't support the encoding keyword parameter.
-verstrline = io.open(VERSIONFILE, encoding='utf-8').read()
+verstrline = open(VERSIONFILE).read()
 VSRE = r'^__version__\s+=\s+[\'"]([^\'"]+)[\'"]'
 mo = re.search(VSRE, verstrline, re.M)
 if mo:
@@ -29,12 +27,11 @@ setup(
 freesms is a Python interface to Free mobile SMS API.""",
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
     ],
